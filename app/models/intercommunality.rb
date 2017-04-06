f4b8e060d3921a8d99ee142fbc0ce955e0a5907b
ejdem <adam_krawiec93@o2.pl>
2017-04-06 22:34:05 +0200

    Apply: Intercommunality passe tous les tests

--- app/models/intercommunality.rb
+++ app/models/intercommunality.rb
@@ -1,7 +1,8 @@
 class Intercommunality < ApplicationRecord
   has_many :communes
   
-  validates :name, presence: true
-  validates :siren, presence: true, uniqueness: true
-  
+  validates :name,  presence: true
+  validates :form,  inclusion: { in: %w(ca cu cc met) }
+  validates :siren, presence: true, length: { is: 9 }
+  validates_uniqueness_of :siren, case_sensitive: false
 end
