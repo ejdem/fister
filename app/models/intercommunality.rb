576f3dcc902c3a6386dca2cb311c870feccc384a
ejdem <adam_krawiec93@o2.pl>
2017-04-07 22:39:27 +0200

    Apply: tests for intercomunality pass

--- app/models/intercommunality.rb
+++ app/models/intercommunality.rb
@@ -5,4 +5,18 @@ class Intercommunality < ApplicationRecord
   validates :form,  inclusion: { in: %w(ca cu cc met) }
   validates :siren, presence: true, length: { is: 9 }
   validates_uniqueness_of :siren, case_sensitive: false
+  
+  before_save :generate_slug
+  
+  def communes_hash
+    {}.tap do |hash|
+      communes.each do |commune|
+        hash.store(commune.code_insee, commune.name)
+      end
+    end
+  end
+  
+  def generate_slug
+    self[:slug] = name.parameterize.downcase.tr(' ','-') if self[:slug].nil?
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170406200643) do
+ActiveRecord::Schema.define(version: 20170407173755) do
 
   create_table "communes", force: :cascade do |t|
     t.string   "name"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20170406200643) do
     t.string   "form"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "slug"
   end
 
   create_table "street_communes", force: :cascade do |t|
