1a99725e0a9941a524d88f7bbce0926c99bceb2f
ejdem <adam_krawiec93@o2.pl>
2017-04-07 19:18:40 +0200

    Apply: Models created, validations and relations for street done, all tests passed

--- app/models/street.rb
+++ app/models/street.rb
@@ -3,5 +3,11 @@ class Street < ApplicationRecord
   has_many :communes, through: :street_communes
   
   validates :title, presence: true
+  validates :from, numericality: true, allow_nil: true
+  validates :to, numericality: true, allow_nil: true
+  validate :to_greater_than_from
   
+  def to_greater_than_from
+    errors.add(:to, "Can't be lower than 'from'") if to && from &&  to < from
+  end
 end
